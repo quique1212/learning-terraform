d4b18923a8a1727b5230d367805cb7fdd489a02b
quique1212 <85503866+quique1212@users.noreply.github.com>
2023-03-10 16:28:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-  subnet_id = "subnet-05a84c31f489fdcc4"
 
   tags = {
     Name = "HelloWorld"
