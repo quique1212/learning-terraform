fbe9d78967bc84604fc045d50de0fb493c38d4fd
quique1212 <85503866+quique1212@users.noreply.github.com>
2023-03-13 18:22:16 +0100

    use module for sg

--- main.tf
+++ main.tf
@@ -19,11 +19,27 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
   subnet_id     = "subnet-05a84c31f489fdcc4"
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [modul.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
   }
+}
+
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.1"
+  name = "blog_new"
+  
+  vpc_id = "vpc-07e528a3bb7699f41"
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
 }
 
 resource "aws_security_group" "blog" {
