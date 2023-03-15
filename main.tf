4f633ca833c725bbf50552abe1de8b1f7693af30
quique1212 <85503866+quique1212@users.noreply.github.com>
2023-03-15 15:26:25 +0100

    create dev vpc

--- main.tf
+++ main.tf
@@ -14,6 +14,23 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -32,7 +49,7 @@ module "blog_sg" {
   version = "4.17.1"
   name = "blog"
   
-  vpc_id = "vpc-07e528a3bb7699f41"
+  vpc_id = module.vpc.public.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
